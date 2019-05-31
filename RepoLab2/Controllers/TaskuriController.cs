0cae5a8d84e8531345ba68f34b4d80c1355017c4
norbertkadar <norbert.kadar.a@gmail.com>
2019-05-31 20:56:01 +0300

    InitialCOmmit

---
+++ RepoLab2/Controllers/TaskuriController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RepoLab2.Models;
+using RepoLab2.Models.DataContext;
+
+namespace RepoLab2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TaskuriController : ControllerBase
+    {
+        private TaskuriDbContext context;
+        public TaskuriController(TaskuriDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Tasks
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpGet]
+        public IEnumerable<Taskul> Get([FromQuery]DateTime from, [FromQuery]DateTime to)
+        {
+            IQueryable<Taskul> result = context.Taskuri;
+            if (from == null && to == null)
+            {
+                return result;
+            }
+            if (from != null)
+            {
+                result = result.Where(p => p.Deadline >= from);
+            }
+            if (to != null)
+            {
+                result = result.Where(p => p.Deadline <= to);
+            }
+            return result;
+
+        }
+
+        // GET: api/Tasks/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            var existing = context.Taskuri
+                .Include(p => p.Comments)
+                .FirstOrDefault(t => t.Id == id);
+            if (existing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(existing);
+        }
+
+        //POST: api/Tasks
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public void Post([FromBody] Taskul task)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Taskuri.Add(task);
+                context.SaveChanges();
+            }
+        }
+
+        // PUT: api/Tasks/5
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Put(int id, [FromBody] Taskul task)
+        {
+            var exists = context.Taskuri.AsNoTracking().FirstOrDefault(t => t.Id == id);
+            if (exists == null)
+            {
+                context.Taskuri.Add(task);
+                context.SaveChanges();
+                return Ok(task);
+            }
+
+            if (task.StatusId.Equals(3))
+            {
+                DateTime localDate = DateTime.Now;
+                task.ClosedAt = localDate;
+            }
+            else
+            {
+                task.ClosedAt = DateTime.MaxValue;
+            }
+
+            context.Taskuri.Update(task);
+            context.SaveChanges();
+            return Ok(task);
+        }
+
+        //Delete: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var exists = context.Taskuri.AsNoTracking().FirstOrDefault(t => t.Id == id);
+            if (exists == null)
+            {
+                return NotFound();
+            }
+            context.Taskuri.Remove(exists);
+            context.SaveChanges();
+            return Ok();
+        }
+
+    }
+}
---
+++ RepoLab2/Models/DataContext/TaskuriDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RepoLab2.Models.DataContext
+{
+    public class TaskuriDbContext : DbContext
+    {
+        public TaskuriDbContext(DbContextOptions<TaskuriDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Taskul> Taskuri { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<TaskImportance> TasksImportance { get; set; }
+    }
+}
